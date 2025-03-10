256fcde3ae53d57ceccc3d36b70c71b78da73b7b
SZF-JamesD <052277@edu.szf.at>
2025-03-10 10:14:21 +0100

    fixed gitignore

--- LE_07/Ex0204Test/Fakes/FakeLogic.cs
+++
@@ -1,6 +0,0 @@
-private void InjectFakeLogicPuzzle(LogicPuzzle fakeLogicPuzzle)
-{
-    var field = typeof(PuzzleService)
-        .GetField("_logicPuzzle", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-    field.SetValue(_puzzleService, fakeLogicPuzzle);
-}
--- LE_07/Ex0204Test/Fakes/FakeMath.cs
+++
@@ -1,6 +0,0 @@
-private void InjectFakeMathPuzzle(MathPuzzle fakeMathPuzzle)
-{
-    var field = typeof(PuzzleService)
-        .GetField("_mathPuzzle", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-    field.SetValue(_puzzleService, fakeMathPuzzle);
-}
